77aef642efb45902f2f0c6744ac56a3ef1dffe91
Abhishek Shrivastav <147899930+AbhishekShrivastav73@users.noreply.github.com>
2024-11-03 18:26:52 +0000

    RazorPay Code

---
+++ RazorPay/routes/indexRoutes.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+const Razorpay = require("razorpay");
+const Payment = require("../models/Payments");
+require("dotenv").config();
+
+const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
+
+router.get("/", function (req, res) {
+  res.render("index");
+});
+
+router.post("/create/orderId", async (req, res) => {
+  const options = {
+    amount: 5000 * 100, // amount in smallest currency unit
+    currency: "INR",
+  };
+  try {
+    const order = await razorpay.orders.create(options);
+    res.send(order);
+
+    const newPayment = await Payment.create({
+      orderId: order.id,
+      amount: order.amount,
+      currency: order.currency,
+      status: "pending",
+    });
+  } catch (error) {
+    res.status(500).send("Error creating order");
+  }
+});
+
+module.exports = router;
